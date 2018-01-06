dcbd509d0ee3313b3b3a0c4485fca997bef5ff6b
Mena Gabara <menagabara2222@gmail.com>
2018-01-06 20:36:44 +0200

    this is v.2

--- js/car.js
+++ js/car.js
@@ -2,7 +2,7 @@ class car{
   constructor(loc) {
     this._model=0
     this._speed=0
-    this._location=loc;
+    this._location=loc
     this._state="before birth"
   }
   set speed(s){
@@ -20,30 +20,27 @@ class car{
   get speed(){
   	return this._speed
   }
-  get state()
-  {
+  get state(){
   	return this._state
   }
-  get location()
-  {
+  get location(){
   	return this._location
   }
-  get model()
-  {
+  get model(){
   	return this._model
   }
 }
+
 class playerCar extends car{
 
-  constructor()
-  {
+  constructor(){
     super({x:(window.innerWidth/2-13),y:window.innerHeight-100})
     this._carI=new Image()
+//    this._fire=new Image()
     this.draw()
 
   }
-  draw()
-  {
+  draw(){
     let canvas=document.getElementById('myCanvas2');
     var ctx2 = canvas.getContext("2d")
     this._carI.src="img/Pink-Car.png"
@@ -52,25 +49,38 @@ class playerCar extends car{
     this.move("right")
   }
 
-  move(direction)
-  {
+  move(direction){
     let canvas=document.getElementById('myCanvas2');
     var ctx2 = canvas.getContext("2d")
-    if(direction=="right")
-    {
-     ctx2.clearRect(this._location.x,this._location.y, 70, 120);
-
-      ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x+10,this._location.y,50,100);
+    if(direction=="right"){
+      ctx2.clearRect(this._location.x,this._location.y, 70, 120)
+      ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x+10,this._location.y,50,100)
       this.location.x+=10
     }
-    else
-    {
-      ctx2.clearRect(this._location.x,this._location.y, 70, 120);
-       ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x-10,this._location.y,50,100);
+    else if (direction=="down"){
+      ctx2.clearRect(this._location.x,this._location.y, 70, 120)
+      ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x,this._location.y+10,50,100)
+      this.location.y+=10
+    }
+    else if(direction=="up"){
+      ctx2.clearRect(this._location.x,this._location.y, 70, 120)
+      ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x,this._location.y-10,50,100)
+      this.location.y-=10
+    }
+    else if(direction=="left"){
+       ctx2.clearRect(this._location.x,this._location.y, 70, 120)
+       ctx2.drawImage(this._carI,200,100,this._carI.width,this._carI.height,this._location.x-10,this._location.y,50,100)
        this.location.x-=10
+    }else{
+      ctx2.clearRect(this._location.x,this._location.y, 70, 120)
+      ctx2.drawImage("img/green.png",200,100,this._carI.width,this._carI.height,this._location.x-10,this._location.y,50,100)
+
     }
   }
-
-
-
 }
+
+
+
+
+    // var myArray = ['left', 'right']
+    // var rand = myArray[Math.floor(Math.random() * myArray.length)]
--- js/game.js
+++ js/game.js
@@ -1,31 +1,84 @@
+x=0
+y=0
+// enemy_imgs=['img/red.png','img/blue.png','img/green.png','img/yellow.png']
+// bonus_imgs=['img/time.png','img/money.png','img/gas.png']
+
 let canvas=document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 let canvas2=document.getElementById('myCanvas2');
 ctx2 = canvas.getContext("2d");
-window.onload = function init()
-{
+let canvas3=document.getElementById('myCanvas3');
+ctx3 = canvas.getContext("2d");
+window.onload = function init(){
   canvas.width=window.innerWidth;
   canvas.height=window.innerHeight;
   canvas2.width=window.innerWidth;
-  canvas2.height=window.innerHeight;
-  var car=new playerCar();
-  var road=new Road();
+  canvas2.height=window.innerHeight
+  canvas3.width=window.innerWidth
+  canvas3.height=window.innerHeight
+  var car=new playerCar()
+  var road=new Road()
+  //var enemy=new enemyCar()
+  game_over=0
   //var audio= new Audio();
   //audio.src="sounds/driving.mp3"
   //audio.play();
   window.addEventListener("keydown", keypress, false);
-  function keypress(event)
-  {
-  	console.log(event.keyCode);
-    //left
-  	if(event.keyCode == 37)
-  	{
-  		car.move("left")
-  	}
-    //right
-  	if(event.keyCode == 39)
-  	{
-  		car.move("right")
-  	}
+  function keypress(event){
+    if(game_over===0){
+    	console.log(event.keyCode);
+      //left
+    	if(event.keyCode == 37)
+    	{
+    		car.move("left")
+        x--
+        console.log(x)
+        if(x<-10){
+          car.move("right")
+          x++
+        }
+    	}
+      //right
+    	if(event.keyCode == 39)
+    	{
+    		car.move("right")
+        x++
+        console.log(x)
+        if(x>10){
+          car.move("left")
+          x--
+        }
+    	}
+      //up
+      if(event.keyCode == 38)
+    	{
+    		car.move("up")
+        y++
+        console.log(y)
+        if(y>23){
+          car.move("down")
+          y--
+        }
+    	}
+      //down
+      if(event.keyCode == 40)
+    	{
+    		car.move("down")
+        y--
+        console.log(y)
+        if(y<0){
+          car.move("up")
+          y++
+        }
+    	}
+  }else{
+    if(event.keyCode===37||event.keyCode===38||event.keyCode===39||event.keyCode===40){
+      car.move("false")
+      x=0
+      y=0
+    }
   }
 }
+}
+//get bonus()
+//collision()
