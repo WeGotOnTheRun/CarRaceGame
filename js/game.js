70bd2ba5d613c1d107e0492339bebd85109097fe
YasmineHamdy <yasminelhosiney@outlook.com>
2018-01-06 09:24:34 +0200

    using canvas

---
+++ js/game.js
@@ -0,0 +1,8 @@
+let canvas=document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+window.onload = function init()
+{
+  canvas.width=window.innerWidth;
+  canvas.height=window.innerHeight;
+
+}
--- js/road.js
+++ js/road.js
@@ -9,15 +9,23 @@ constructor(themeId,speed) {
   }
   draw()
   {
+    let canvas=document.getElementById('myCanvas');
+    var background = new Image();
     var c=0;
      var themes=["img/Picture5.png","img/Picture6.png","img/Picture7.png"]
 
     this._timer=setInterval(
       function(){
-        document.body.style.backgroundImage ="url("+themes[c]+")";
-        c=(c+1)%3
+
+        background.src = themes[c]
+        ctx.drawImage(background,0,0,background.width,background.height,0,0,canvas.width,canvas.height);
+            c=(c+1)%3
     },
      500);
+  }
+  stopTimer()
+  {
+    clearInterval(this._timer);
   }
   set themes(th)
   {
